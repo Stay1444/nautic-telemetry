66053036d5b43abe45b58187dc6590ebb2aeaf79
Stay <qalonso@zaroz.es>
2024-05-01 12:46:04 +0200

    trimStart header

--- boat/src/Utils.h
+++ boat/src/Utils.h
@@ -6,7 +6,8 @@
 namespace utils {
 namespace arrays {
 void copy(const uint8_t *source, uint8_t *destination, size_t length);
-}
+void trimStart(uint8_t *source, size_t trimLength, size_t totalLength);
+} // namespace arrays
 namespace conversions {
 size_t bytesToSizeT(const uint8_t *bytes);
 uint32_t bytesToUint32(const uint8_t *bytes);
